030cd2d0851d3442fe437bcb993c0460d9bf3987
sohaibroy <157553837+sohaibroy@users.noreply.github.com>
2025-06-25 13:14:07 -0600

    a

--- backend/public/index.php
+++ backend/public/index.php
@@ -31,7 +31,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 // |
 // */
 
- require __DIR__.'/../vendor/autoload.php';
+ //require __DIR__.'/../vendor/autoload.php';
 
 // /*
 // |--------------------------------------------------------------------------
@@ -54,4 +54,4 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 
 // $kernel->terminate($request, $response);
 
-echo 'FROM THE APP INDEX.PHP';
+//echo 'FROM THE APP INDEX.PHP';
