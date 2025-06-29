67bbf1b7f549612578181adb3f11d7b1edd6b826
sohaibroy <157553837+sohaibroy@users.noreply.github.com>
2025-06-28 23:11:21 -0600

    ok i think its good

--- frontend/app/admin-login/page.jsx
+++ frontend/app/admin-login/page.jsx
@@ -4,21 +4,6 @@ import React from "react";
 
 // this is the "Admin Login" Button - not the actual page
 
-// function AdminLogin() {
-//   return (
-//     <div className="flex justify-center items-center h-[70vh]">
-//       <Login
-//         title="Admin Login"
-//         link="./admin-selection/"
-//         // apiPath="http://localhost:80/api/login/admin"
-//         apiPath={`${baseUrl}/api/login/admin`}
-//       />
-//     </div>
-//   );
-// }
-
-// export default AdminLogin;
-
 function AdminLogin() {
   return (
     <div className="flex justify-center items-center h-[70vh]">
