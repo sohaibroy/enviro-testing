06170706bdde652aefde37350e157f63ff494035
sohaibroy <157553837+sohaibroy@users.noreply.github.com>
2025-06-28 22:43:13 -0600

    dsd

--- frontend/app/admin-login/page.jsx
+++ frontend/app/admin-login/page.jsx
@@ -1,7 +1,23 @@
 import { Login } from "../components/login/Login";
 const baseUrl = process.env.NEXT_PUBLIC_API_URL;  //added
+import React from "react";
 
 // this is the "Admin Login" Button - not the actual page
+
+// function AdminLogin() {
+//   return (
+//     <div className="flex justify-center items-center h-[70vh]">
+//       <Login
+//         title="Admin Login"
+//         link="./admin-selection/"
+//         // apiPath="http://localhost:80/api/login/admin"
+//         apiPath={`${baseUrl}/api/login/admin`}
+//       />
+//     </div>
+//   );
+// }
+
+// export default AdminLogin;
 
 function AdminLogin() {
   return (
@@ -9,8 +25,8 @@ function AdminLogin() {
       <Login
         title="Admin Login"
         link="./admin-selection/"
-        // apiPath="http://localhost:80/api/login/admin"
         apiPath={`${baseUrl}/api/login/admin`}
+        isAdmin={true}
       />
     </div>
   );
