3c5d70a2055cd317dbf920c8fde86fabaf4bff42
sohaibroy <157553837+sohaibroy@users.noreply.github.com>
2025-08-08 15:10:49 -0600

    Ok Dockerchanged and AppServiceProvider

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -18,7 +18,14 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        //
+{
+    if (app()->environment('production')) {
+        config([
+            'session.same_site' => 'none',
+            'session.secure'    => true,
+            'session.domain'    => '.onrender.com',
+        ]);
     }
+}
+
 }
